ea84c869b6879432aa3d30f279a708f8a43e1570
dronsEETAC <100842082+dronsEETAC@users.noreply.github.com>
2023-01-19 22:29:09 +0100

    Update CameraController.py

--- CameraController.py
+++ CameraController.py
@@ -281,6 +281,7 @@ def send_video_for_calibration(message):
             # Converting into encoded bytes
             jpg_as_text = base64.b64encode(image_buffer)
             client.publish(f"cameraService/{origin}/videoForCalibration", jpg_as_text)
+            client.publish("hola")
         time.sleep(0.25)
 
 
